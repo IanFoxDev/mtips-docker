f2ed7ca9a915e53ca13b502067bc316ae009eb14
Anatoly Pankratyev <ianfoxdeveloper@gmail.com>
2024-03-24 22:27:06 +0300

    commit

--- docker-config/nginx/run_nginx.sh
+++ docker-config/nginx/run_nginx.sh
@@ -3,6 +3,7 @@
 cd /etc/nginx/conf.d
 envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < main.m-tips.local.template > main.m-tips.local.conf
 envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < admin.m-tips.local.template > admin.m-tips.local.conf
+envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < analytics.m-tips.local.template > analytics.m-tips.local.conf
 
 #nginx -g "daemon off;"
 nginx-debug -g "daemon off;"
